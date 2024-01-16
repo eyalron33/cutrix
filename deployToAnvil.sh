a82f84dc9546019e6108ecf4815fa736ae0be2e8
neiman <eyalron33@gmail.com>
2024-01-16 17:11:25 +0100

    print a matrix with the first character of the address in the first cell

---
+++ deployToAnvil.sh
@@ -0,0 +1,12 @@
+#! /usr/bin/bash
+YELLOW='\033[1;33m'
+NC='\033[0m' # No Color
+
+
+cd "$(dirname "$0")"
+
+# add local variables to scope
+source .env
+
+echo -e "${YELLOW}deply Cutrix${NC}"
+forge create --rpc-url http://127.0.0.1:8545/ --private-key $PRIVATE_KEY_ANVIL src/Cutrix.sol:Cutrix 
--- src/Cutrix.sol
+++ src/Cutrix.sol
@@ -14,10 +14,13 @@ contract Cutrix is ERC721URIStorage {
         _mint(msg.sender, tokenID);
     }
 
-    function generateCutrix(uint256 tokenId) public view returns(string memory){
+    function generateCutrix(uint256 tokenId) public pure returns(string memory){
+        bytes1 lastChar = getFirstCharacter(uint256ToAddress(tokenId));
 
         bytes memory svg = abi.encodePacked(
-            '<svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="5 5 55 55" stroke="#000"> <path fill="white" stroke-width="3" d="M10 10h50v50H10z"/> <path fill="white" d="M11 11h12v12H11zM11 23h12v12H11zM11 35h12v12H11zM11 47h12v12H11zM23 11h12v12H23zM23 23h12v12H23zM23 35h12v12H23zM23 47h12v12H23zM35 11h12v12H35zM35 23h12v12H35zM35 35h12v12H35zM35 47h12v12H35zM47 11h12v12H47zM47 23h12v12H47zM47 35h12v12H47zM47 47h12v12H47z"/> </svg>'
+            '<svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="5 5 55 55" stroke="#000"> <path fill="white" stroke-width="3" d="M10 10h50v50H10z"/> <path fill="white" d="M11 11h12v12H11zM11 23h12v12H11zM11 35h12v12H11zM11 47h12v12H11zM23 11h12v12H23zM23 23h12v12H23zM23 35h12v12H23zM23 47h12v12H23zM35 11h12v12H35zM35 23h12v12H35zM35 35h12v12H35zM35 47h12v12H35zM47 11h12v12H47zM47 23h12v12H47zM47 35h12v12H47zM47 47h12v12H47z"/> <text x="12" y="21" font-size="12">',
+                lastChar,
+                '</text></svg>'
         );
 
         return string(
@@ -42,6 +45,42 @@ contract Cutrix is ERC721URIStorage {
                 Base64.encode(dataURI)
             )
         );
+    }
+
+    // Function to return the first character of an address
+    function getFirstCharacter(address addr) internal pure returns (bytes1) {
+        // Convert the address to a string
+        string memory addressString = addressToString(addr);
+
+        // Get the first character after "0x"
+        bytes1 firstCharacter = bytes(addressString)[2];
+
+        return firstCharacter;
+    }
+
+    // Function to convert address to string
+    function addressToString(address addr) internal pure returns (string memory) {
+        bytes32 value = bytes32(uint256(uint160(addr)));
+        bytes memory alphabet = "0123456789abcdef";
+
+        bytes memory str = new bytes(42);
+        str[0] = "0";
+        str[1] = "x";
+
+        for (uint256 i = 0; i < 20; i++) {
+            str[2 + i * 2] = alphabet[uint8(value[i + 12] >> 4)];
+            str[3 + i * 2] = alphabet[uint8(value[i + 12] & 0x0f)];
+        }
+
+        return string(str);
+    }
+
+    function addressToUint256(address addr) public pure returns (uint256) {
+        return uint256(uint160(addr));
+    }
+
+    function uint256ToAddress(uint256 addrAsUint256) public pure returns (address) {
+        return address(uint160(addrAsUint256));
     }
    
 }
