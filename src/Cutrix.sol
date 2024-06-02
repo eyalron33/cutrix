bd334e6eb02f18bf81809d373bf962843d69421f
neiman <eyalron33@gmail.com>
2024-06-02 23:37:49 +0200

    add mintPrice including getter and setter. TokenURI now returns data only for existing tokens (minted and not burned). Add ERC721Enumerable to Cutrix

--- src/Cutrix.sol
+++ src/Cutrix.sol
@@ -20,6 +20,7 @@ contract Cutrix is  ERC721URIStorage, ERC721Enumerable, Ownable {
         uint8 flags; // 1 bit underlink, 1 overline, 1 blnking, rest is garbage bits
     }
 
+    uint256 public mintPrice;
     ICutrixData internal cutrixData;
 
     constructor(ICutrixData cutrixDataContructor) ERC721("Cutrix", "CTRX") Ownable(msg.sender) {
@@ -28,14 +29,19 @@ contract Cutrix is  ERC721URIStorage, ERC721Enumerable, Ownable {
 
     // The tokenID is actually the uint256 representation of an address.
     // Anyone can mint a token, but the ownership will go to the address that tokenID represents.
-    function mint(uint256 tokenID) external {
+    public function mint(uint256 tokenID) external {
+        require(msg.value >= mintPrice, "Cutrix: insufficient funds");
+
         uint160 Uint160Address = uint160(tokenID);
         address tokenIDAddress = address(Uint160Address);
 
         _mint(tokenIDAddress, tokenID);
     }
 
+    // Returns the tokenURI for the specified tokenID.
     function tokenURI(uint256 tokenID) public view override(ERC721, ERC721URIStorage) returns (string memory){
+        require(ownerOf(tokenID) != address(0), "The cutrix is either not minted or was burned");
+
         bytes memory dataURI = abi.encodePacked(
             '{',
                 '"name": "Cutrix #', Strings.toString(tokenID), '",',
@@ -50,6 +56,23 @@ contract Cutrix is  ERC721URIStorage, ERC721Enumerable, Ownable {
                 Base64.encode(dataURI)
             )
         );
+    }
+
+    // Sets a new mint price for the tokens.
+    function setMintPrice(uint256 newMintPrice) onlyOwner() {
+        mintPrice = newMintPrice;
+    }
+
+    function _increaseBalance(address account, uint128 value) internal override(ERC721, ERC721Enumerable) {
+        super._increaseBalance(account, value);
+    }
+
+    function _update(address to, uint256 tokenID, address auth) internal override(ERC721, ERC721Enumerable) returns (address) {
+        return super._update(to, tokenID, auth);
+    }
+
+    function supportsInterface(bytes4 interfaceId) public view virtual override(ERC721Enumerable, ERC721URIStorage) returns (bool) {
+        return super.supportsInterface(interfaceId);
     }
 
 }
